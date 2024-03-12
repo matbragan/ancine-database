2014bf47c2be4532f685fe2261dca412f25a0287
matbragan <matheus.bs.oliveira@gmail.com>
2024-03-12 17:43:51 -0300

    add download py script

---
+++ ancine/download.py
@@ -0,0 +1,56 @@
+
+import logging
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+BASE_URL = 'https://dados.ancine.gov.br/dados-abertos/'
+
+FILES = [
+    'crt-obras-nao-publicitarias-csv.zip',
+    'obras-nao-pub-brasileiras-csv.zip',
+    'obras-nao-pub-estrangeiras-csv.zip',
+    'bilheteria-diaria-obras-por-distribuidoras-csv.zip',
+    # 'salas-de-exibicao-e-complexos.csv'
+]
+
+
+def download_and_unzip(base_url, file):
+    '''
+    Função para fazer download e descompactar os arquivos da Ancine
+
+    args:
+        - base_url: url base para download
+        - file: arquivo que completa a url para download
+    '''
+    try:
+        url = base_url + file
+        http_response = urlopen(url)
+        zipfile = ZipFile(BytesIO(http_response.read()))
+
+        msg = f'{file} - zip file read successfully'
+        log.info(msg)
+
+        prefix_file = file.split('-csv')[0]
+        extract_to = f'./downloads/{prefix_file}'
+        zipfile.extractall(path=extract_to)
+
+        msg = f'{file} - unzip & download file successfully'
+        log.info(msg)
+    except Exception as e:
+        msg = f'{file} - {e}'
+        log.error(msg)
+
+
+if __name__ == '__main__':
+    for file in FILES:
+        download_and_unzip(BASE_URL, file)
+
+
+
+
+
