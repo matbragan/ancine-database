68182e2d0c20359b4dc46d8ca1cfa6c0230d0917
matbragan <matheus.bs.oliveira@gmail.com>
2024-03-22 03:05:26 -0300

    update log message format

--- ancine/download.py
+++ ancine/download.py
@@ -3,7 +3,8 @@ from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
 
-logging.basicConfig(level=logging.INFO)
+log_format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_format)
 log = logging.getLogger(__name__)
 
 
