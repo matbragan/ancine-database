6f6ef9969b3c5dcf090d73a78cbf498c4ce6df59
matbragan <matheus.bs.oliveira@gmail.com>
2024-03-22 21:33:54 -0300

    improve convert database script

--- ancine/convert_database.py
+++ ancine/convert_database.py
@@ -1,10 +1,16 @@
 import os
+import logging
 
 from dotenv import load_dotenv
 import pandas as pd
 from sqlalchemy import create_engine
 
+log_format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_format)
+log = logging.getLogger(__name__)
+
 load_dotenv()
+
 
 FILES = [
     'bilheteria-diaria-obras-por-distribuidoras',
@@ -41,8 +47,34 @@ def connection_string() -> str:
     return f'mysql://{user}:{pwd}@{host}:{port}/{database}'
 
 
+def create_con() -> create_engine:
+    try:
+        engine = create_engine(connection_string())
+        msg = 'Conectado ao banco MySQL'
+        log.info(msg)
+        return engine
+    except Exception as e:
+        msg = f'Erro ao conectar ao banco - {e}'
+        log.error(msg)
+
+
+def load_table(df: pd.DataFrame, engine: create_engine, table_name: str) -> None:
+    try:
+        df.to_sql(name=table_name, con=engine, if_exists='replace')
+        msg = f'Tabela {table_name} carregada no banco MySQL com sucesso'
+        log.info(msg)
+    except Exception as e:
+        msg = f'Erro ao carregar a tabela {table_name} no banco MySQL - {e}'
+        log.error(msg)
+
+
+def close_con(engine) -> None:
+    engine.dispose()
+
+
 if __name__ == '__main__':
     path = "downloads/crt-obras-nao-publicitarias/"
     df = read_csvs(path)
-    engine = create_engine(connection_string())
-    df.to_sql(name='titulos', con=engine, if_exists='replace')
+    engine = create_con()
+    load_table(df, engine, 'titulos')
+    close_con(engine)
